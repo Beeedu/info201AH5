34c26b79f2749b5e085090d31ce1c6df9ba733e0
blynch <bralyn99@gmail.com>
2019-11-18 01:34:44 -0800

    Set up base for Shiny UI

--- analysis.R
+++ analysis.R
@@ -1,5 +1,7 @@
 # This document is where data wrangling will take place.
 
+library("dplyr")
+
 # Function to get started with a fresh data frame.
 get_emissions_data <- function() {
   read.csv("co2-emissions-by-nation.csv", stringsAsFactors = FALSE)
--- app.R
+++ app.R
@@ -1,3 +1,101 @@
 # File for Shiny application.
 
 source("analysis.R")
+
+library("shiny")
+
+background <- tabPanel(
+  "Background",
+  mainPanel(p("Read this from a .txt file?"))
+)
+
+conclusion <- tabPanel(
+  "Conclusion",
+  mainPanel(p("Read this from a .txt file?"))
+)
+
+about_us <- tabPanel(
+  "About Us",
+  mainPanel(p("Read this from a .txt file?"))
+)
+
+visualizations <- tabPanel(
+  "Visualizations",
+  mainPanel(
+    tabPanel(
+      "Plot",
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput(
+            inputId = "yr_range",
+            label = "Year range",
+            min = 1751,
+            max = 2014,
+            value = c(1751, 2014),
+            sep = "",
+          ),
+          # Make this checkbox group drop down, save space
+          # if user is not using the filter
+          checkboxGroupInput(
+            inputId = "checked_countries",
+            label = "Countries",
+            choices = c("United States", "China", "United Kingdom")
+          )
+        ),
+        mainPanel(
+          h3("Plot title"),
+          p("Plot would go here"),
+        )
+      )
+    ),
+    tabPanel(
+      "Map",
+      sidebarLayout(
+        sidebarPanel(
+          # sliderInput(
+          #   inputId = "yr",
+          #   label = "Year",
+          #   min = 1751,
+          #   max = 2014,
+          #   value = 2014,
+          #   sep = "",
+          # )
+          selectInput(
+            inputId = "yr",
+            label = "Year",
+            choices = c(1751:2014),
+            selected = 2014
+          )
+        ),
+        mainPanel(
+          h3("Map title"),
+          p("Map would go here"),
+        )
+      )
+  )
+  )
+)
+
+visTab <- tabPanel("Visualizations", visualizations)
+
+my_ui <- fluidPage(
+  navbarPage(
+    "World Nation Carbon Emissions Data Report",
+    background,
+    visTab,
+    conclusion,
+    about_us
+  )
+)
+
+my_server <- function(input, output) {
+  # world_emissions_map <- renderLeaflet({
+  #   
+  # })
+  
+  # countries_emissions_plot <- renderPlot({
+  #   
+  # })
+}
+
+shinyApp(ui = my_ui, server = my_server)
