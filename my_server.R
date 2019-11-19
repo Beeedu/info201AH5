7a9388e61bf0f32dd63d9345d1af224df6852805
blynch99 <bralyn99@gmail.com>
2019-11-19 15:26:24 -0800

    added ui and server files to source in the app.R file

---
+++ my_server.R
@@ -0,0 +1,11 @@
+library("shiny")
+
+my_server <- function(input, output) {
+  # world_emissions_map <- renderLeaflet({
+  #   
+  # })
+  
+  # countries_emissions_plot <- renderPlot({
+  #   
+  # })
+}
