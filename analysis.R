e4a752c918b67340d607df6fd88a9fed17504c8a
jasonw37 <wuyi20122@gmail.com>
2019-11-20 18:35:00 -0800

    Line plot rendered in Shiny

--- analysis.R
+++ analysis.R
@@ -12,12 +12,15 @@ View(get_emissions_data())
 emissions_data <- get_emissions_data()
 
 # Line graph
-Particular_nation <- filter(emissions_data, Nation == "AFGHANISTAN")
-
-line_graph <- ggplot(data = Particular_nation, mapping = 
-              aes(x = Year, y = Per.capita.CO2.emissions..metric.tons.of.carbon., group = 1)) +
-  geom_line() +
-  geom_point() +
+line_graph <- ggplot() +
+  geom_line(data = filter(emissions_data, Nation == "AFGHANISTAN"),
+            mapping = aes(x = Year,
+                          y = Per.capita.CO2.emissions..metric.tons.of.carbon.,
+                          group = 1), color = "blue") +
+  geom_line(data = filter(emissions_data, Nation == "ALBANIA"),
+            mapping = aes(x = Year,
+                          y = Per.capita.CO2.emissions..metric.tons.of.carbon.,
+                          group = 1), color = "red") +
   labs(
     title = "Emissions of Nation",
     x = "Year",
@@ -26,43 +29,43 @@ line_graph <- ggplot(data = Particular_nation, mapping =
 
 line_graph
 
-# Summary Table
-get_emissions_summary_table <- get_emissions_data() %>%
-  group_by(Nation) %>%
-  summarise(total_emissions = sum(Per_capita_CO2_emissions, na.rm = TRUE)
-            )
-
-View(get_emissions_summary_table)
-
-# Bar Graph
-top_15_nations_emission <- get_emissions_data() %>%
-  mutate(total_emissions = sum(Per_capita_CO2_emissions, na.rm = TRUE)) %>%
-  arrange(-total_emissions) %>%
-  top_n(15)
-plot <- ggplot(top_15_nations_emission) +
-  geom_col(mapping = aes(x = Nation, y = total_emissions)) +
-  coord_flip() +
-  labs(
-    title = "Top 15 emission nations",
-    x = "Nation",
-    y = "Total Emissions"
-  )
-
-plot
-
-# interactive map
-nations_emission_interactive_map <- leaflet(data = emissions_data) %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  setView(lng = 150, lat = 45, zoom = 7) %>%
-  addCircleMarkers(
-    lat = ~lat,
-    lng = ~long,
-    stroke = FALSE,
-    popup = ~paste0("Year: ", Year, "<br>",
-                    "Nation: ", Nation, "<br>",
-                    "Emissions Per Capita: ", Per_capita_CO2_emissions),
-    radius = ~ (Per_capita_CO2_emissions),
-    fillOpacity = 0.5,
-  )
-
-nations_emission_interactive_map
+# # Summary Table
+# get_emissions_summary_table <- emissions_data %>%
+#   group_by(Nation) %>%
+#   summarise(total_emissions = sum(Per.capita.CO2.emissions..metric.tons.of.carbon., na.rm = TRUE)
+#             )
+# 
+# View(get_emissions_summary_table)
+# 
+# # Bar Graph
+# top_15_nations_emission <- emissions_data %>%
+#   mutate(total_emissions = sum(Per.capita.CO2.emissions..metric.tons.of.carbon., na.rm = TRUE)) %>%
+#   arrange(-total_emissions) %>%
+#   top_n(15)
+# plot <- ggplot(top_15_nations_emission) +
+#   geom_col(mapping = aes(x = Nation, y = total_emissions)) +
+#   coord_flip() +
+#   labs(
+#     title = "Top 15 emission nations",
+#     x = "Nation",
+#     y = "Total Emissions"
+#   )
+# 
+# plot
+# 
+# # interactive map
+# nations_emission_interactive_map <- leaflet(data = emissions_data) %>%
+#   addProviderTiles("CartoDB.Positron") %>%
+#   setView(lng = 150, lat = 45, zoom = 7) %>%
+#   addCircleMarkers(
+#     lat = ~lat,
+#     lng = ~long,
+#     stroke = FALSE,
+#     popup = ~paste0("Year: ", Year, "<br>",
+#                     "Nation: ", Nation, "<br>",
+#                     "Emissions Per Capita: ", Per.capita.CO2.emissions..metric.tons.of.carbon.),
+#     radius = ~ (Per.capita.CO2.emissions..metric.tons.of.carbon.),
+#     fillOpacity = 0.5,
+#   )
+# 
+# nations_emission_interactive_map
--- my_server.R
+++ my_server.R
@@ -1,11 +1,26 @@
 library("shiny")
+library("ggplot2")
+source("analysis.R")
 
 my_server <- function(input, output) {
   # world_emissions_map <- renderLeaflet({
   #   
   # })
   
-  # countries_emissions_plot <- renderPlot({
-  #   
-  # })
+output$countries_emissions_plot <- renderPlot( 
+  ggplot() +
+    geom_line(data = filter(emissions_data, Nation == "AFGHANISTAN"),
+              mapping = aes(x = Year,
+                            y = Per.capita.CO2.emissions..metric.tons.of.carbon.,
+                            group = 1), color = "blue") +
+    geom_line(data = filter(emissions_data, Nation == "ALBANIA"),
+              mapping = aes(x = Year,
+                            y = Per.capita.CO2.emissions..metric.tons.of.carbon.,
+                            group = 1), color = "red") +
+    labs(
+      title = "Emissions of Nation",
+      x = "Year",
+      y = "Emissions Per Capita"
+    )
+)
 }
